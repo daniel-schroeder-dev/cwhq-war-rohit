d8306d284d25a14f0a1e74bcfc1d0090ef29e00e
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-08-03 12:12:15 -0500

    Added basic ranks and suits.

---
+++ main.py
@@ -0,0 +1,17 @@
+card = {
+    "suit": "Clubs",
+    "rank": "Ace",
+    "value": 13,
+}
+
+card["suit"] = "clubs" 
+card["rank"] = "Ace"
+card["value"] = 13 # should this be a String?
+
+
+ranks = "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
+
+suits = "Clubs","Hearts","Diamonds","Spades"
+values = []
+
+my_name = "Daniel Schroeder"
