d02b4a396d5979676c824aba238c0fe367e0ff41
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-08-03 12:46:20 -0500

    Got suit and rank appeded to cards.

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-
-card = {
-    "suit": "Clubs",
-    "rank": "Ace",
-    "value": 13,
-}
-
-card["suit"] = "clubs" 
-card["rank"] = "Ace"
-card["value"] = 13 # should this be a String?
-
+card = {}
 
 ranks = "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
 suits = "Clubs","Hearts","Diamonds","Spades"
 values = tuple(range(2,14))
 
+cards = []
+for rank in ranks:
+    for suit in suits:
+        cards.append({ "suit": suit, "rank": rank })
 
+
+print(cards)
