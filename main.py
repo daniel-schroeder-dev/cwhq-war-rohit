dffcd6a2e6849b183ecfb2d9e5b6272e6f5b73af
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-08-05 16:22:43 -0500

    Added rank_to_value zip loop.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+
 from pprint import pprint
 card = {}
 
 ranks = "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
 suits = "Clubs","Hearts","Diamonds","Spades"
-values = tuple(range(2,14))
+values = tuple(range(2,15)) 
+
+rank_to_value = {}
+for rank, value in zip(ranks, values):
+    rank_to_value[rank] = value
+
 
 cards = []
 for rank in ranks:
@@ -11,4 +17,3 @@ for rank in ranks:
         cards.append({ "suit": suit, "rank": rank })
 
 
-pprint(cards)
