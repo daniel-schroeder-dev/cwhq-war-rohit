a590a4abad4e8113c1ccd563f961efdd0405c23e
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-08-05 16:37:43 -0500

    Deck of cards is built.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-
 from pprint import pprint
-card = {}
 
 ranks = "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
 suits = "Clubs","Hearts","Diamonds","Spades"
@@ -10,10 +8,10 @@ rank_to_value = {}
 for rank, value in zip(ranks, values):
     rank_to_value[rank] = value
 
-
 cards = []
 for rank in ranks:
-    for suit in suits:
-        cards.append({ "suit": suit, "rank": rank })
+    for suit in suits:                       
+        cards.append({ "suit": suit, "rank": rank, "value": rank_to_value[rank] })
 
 
+pprint(cards)
