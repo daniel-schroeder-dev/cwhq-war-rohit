0dfd032509f1a8346c292df6426c4f0559ca1524
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-08-11 16:35:55 -0500

    Finished comparing cards.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 import signal, sys
-
+import random
+import time as slp
 
 def exit_handler(sig, frame):
     print("\nGoodbye!")
@@ -23,7 +24,7 @@ for rank in ranks:
         cards.append({ "suit": suit, "rank": rank, "value": rank_to_value[rank] })
 
 
-pprint(cards)
+# pprint(cards)
 
 """
 In a game loop:
@@ -31,7 +32,31 @@ In a game loop:
   - Learn to split the deck between a player and the computer.
   - Draw and compare the cards for each turn. 
 """
-
+played_cards=[]
 while True:
-
+    random.shuffle(cards)
+    player1 = cards[26:]
+    player2 = cards[:26]
     input("This loops till you press CTRL-C ")
+    current_card = player1.pop()
+    current_card2 = player2.pop()
+    played_cards.append(current_card)
+    played_cards.append(current_card2)
+    print(played_cards)
+
+    if current_card["value"] == current_card2["value"]:
+        print("war")
+   
+    else:
+        winner = max(current_card, current_card2, key=lambda card: card["value"])
+    
+    print(f"winner {winner}")
+
+
+    
+
+    
+    # not for me:
+        # for card1, card2 in zip(played_cards, played_cards2):        
+        #     high_card = max(card1, card2, key=lambda x: x["value"])
+        #     print(f"High card is {high_card}")
